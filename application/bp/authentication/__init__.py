3e419bf07923c265f90707078c394684d3a66830
Konrad <skullzablaze56@gmail.com>
2025-04-22 14:06:02 -0400

    Hw done. There's tons of debugging in here because I kept failing one test. Resubmission

--- application/bp/authentication/__init__.py
+++ application/bp/authentication/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 authentication = Blueprint('authentication', __name__, template_folder='templates')
-
+#testing
 # Import routes at the bottom to avoid circular imports
 from application.bp.authentication import routes
 
