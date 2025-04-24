849e3a15b96aba9314defd1756121a250ebae761
Konrad <skullzablaze56@gmail.com>
2025-04-24 11:48:10 -0400

    fix

--- application/bp/authentication/__init__.py
+++ application/bp/authentication/__init__.py
@@ -1,8 +1,10 @@
+"""Authentication blueprint"""
 from flask import Blueprint
 
-authentication = Blueprint('authentication', __name__, template_folder='templates')
-#testing
-# Import routes at the bottom to avoid circular imports
+authentication = Blueprint('authentication', __name__, 
+                         template_folder='templates')
+
+# Import routes after blueprint creation to avoid circular imports
 from application.bp.authentication import routes
 
 
--- application/bp/authentication/forms/__init__.py
+++ application/bp/authentication/forms/__init__.py
@@ -1,3 +1,4 @@
+"""Authentication forms module"""
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
@@ -5,6 +6,7 @@ from application.database import User
 
 
 class RegistrationForm(FlaskForm):
+    """Registration form"""
     email = StringField('Email',
                        validators=[DataRequired(), 
                                  Email()])
@@ -17,16 +19,13 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self, field):
         """Custom validator for email field"""
-        print(f"\n=== Validating Email ===")
-        print(f"Checking email: {field.data}")
         user = User.query.filter_by(email=field.data).first()
-        print(f"Found user: {user}")
         if user:
-            print("Email already exists - raising ValidationError")
             raise ValidationError('That email is already registered.')
 
 
 class LoginForm(FlaskForm):
+    """Login form"""
     email = StringField('Email',
                        validators=[DataRequired(), 
                                  Email()])
